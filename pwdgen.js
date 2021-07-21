9e9fca16c342ac514c1a774b844f45e6af9d9e2b
Temirlan Zhekenov <temirlan.zhekenov@nu.edu.kz>
2021-07-21 11:41:21 +0600

    MPG_final

--- pwdgen.js
+++
@@ -1,6 +0,0 @@
-var hotpOtpGenerator = require('hotp-totp-generator')
-
-   // Example of TOTP
-  const totpToken = hotpOtpGenerator.totp({ key: 'temirlan.zhekenov@nu.edu.kzDPSCHALLENGE', algorithm: 'sha512',X: '120',T0: '0',digits: '10'});
-  
-  console.log(totpToken)
